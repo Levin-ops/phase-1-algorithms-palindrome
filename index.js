6144a3cf7053c3f305af5cb169735da4c77d65be
Levin Mwanganyi Mwambi <artmwanganyi09@outlook.com>
2023-10-12 11:27:56 +0300

    Palindrome Lab

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const normalWord = word.toLowerCase();
+  const wordInReverse = normalWord.split('').reverse().join('');
+
+  if (normalWord===wordInReverse){
+    return true;
+  }else{
+  return false ;
+  }
 }
 
 /* 
   Add your pseudocode here
-*/
+convert the word to have similar casing ie .toLowerCase() or .toUpperCase()
+if (word[0]===word[-1]{
+  return True
+}else{
+  return False
+}
+}
+  */
 
 /*
   Add written explanation of your solution here
+create 2 normalWord and wordInReverse variables to store the word at function level scope
+the split method creates a new array without altering the original array(normalWord) then
+reverses the order of the elements in the array wordInReverse then joins the elemnts
+
+The if block check for similarity using the strict equality operator and returns false if 
+the words are  similar it returns true and false if they are not similar
 */
 
 // You can run `node index.js` to view these console logs
